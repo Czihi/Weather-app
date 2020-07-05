214c382955f29a24b0c201f261aee8eb0cc121e4
Czihi <boslak@student.agh.edu.pl>
2020-07-05 20:11:14 +0200

    Updates

---
+++ precache-manifest.73cb79c096631efcb86e343b0f3d3b7e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "a76ae2791409b6ef63279e1baa02937b",
+    "url": "/Weather-app/static/media/hot.a76ae279.png"
+  },
+  {
+    "revision": "a19ca12c780699c6d12a",
+    "url": "/Weather-app/static/css/main.3d5916e1.chunk.css"
+  },
+  {
+    "revision": "cd23450c59ffd8f53315",
+    "url": "/Weather-app/static/js/runtime~main.9b6b42b8.js"
+  },
+  {
+    "revision": "19f5e3c9ab01c09ab137948386ed10d1",
+    "url": "/Weather-app/static/media/OWM.19f5e3c9.png"
+  },
+  {
+    "revision": "9d2803f3f6e47f9cf04b",
+    "url": "/Weather-app/static/js/2.0528410a.chunk.js"
+  },
+  {
+    "revision": "d6a6e2338b57d94cb1545930876d0359",
+    "url": "/Weather-app/static/media/cold.d6a6e233.png"
+  },
+  {
+    "revision": "a19ca12c780699c6d12a",
+    "url": "/Weather-app/static/js/main.dfb9bc29.chunk.js"
+  },
+  {
+    "revision": "b828778431750889a3903e7f169af884",
+    "url": "/Weather-app/static/media/hot2.b8287784.png"
+  },
+  {
+    "revision": "16b588aacdead0623d7ea8391a130537",
+    "url": "/Weather-app/static/media/Czihi.16b588aa.png"
+  },
+  {
+    "revision": "ce7bee1f7b4ecadd6694c5abaa0a5c2d",
+    "url": "/Weather-app/static/media/NBP.ce7bee1f.png"
+  },
+  {
+    "revision": "9d2803f3f6e47f9cf04b",
+    "url": "/Weather-app/static/css/2.301cbc31.chunk.css"
+  },
+  {
+    "revision": "83d6679ccf21500ce50afdf9ba3cff9d",
+    "url": "/Weather-app/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Weather-app/precache-manifest.70782a532315e9bc5b01baf3f4f8cc74.js"
+  "/Weather-app/precache-manifest.73cb79c096631efcb86e343b0f3d3b7e.js"
 );
 
 workbox.clientsClaim();

8f1df7915fedaa223437e80db31504279c24fff6
Czihi <boslak@student.agh.edu.pl>
2020-07-05 18:39:19 +0200

    Updates

---
+++ precache-manifest.9b2ddb164b7b9eb4220004990d732229.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "b828778431750889a3903e7f169af884",
+    "url": "/Weather-app/static/media/hot2.b8287784.png"
+  },
+  {
+    "revision": "a76ae2791409b6ef63279e1baa02937b",
+    "url": "/Weather-app/static/media/hot.a76ae279.png"
+  },
+  {
+    "revision": "d6a6e2338b57d94cb1545930876d0359",
+    "url": "/Weather-app/static/media/cold.d6a6e233.png"
+  },
+  {
+    "revision": "cd23450c59ffd8f53315",
+    "url": "/Weather-app/static/js/runtime~main.9b6b42b8.js"
+  },
+  {
+    "revision": "3245a9284d2d4d11a023",
+    "url": "/Weather-app/static/js/main.fc9d0fd1.chunk.js"
+  },
+  {
+    "revision": "a30dd1d7435761ba6020",
+    "url": "/Weather-app/static/js/2.eaedef7a.chunk.js"
+  },
+  {
+    "revision": "3245a9284d2d4d11a023",
+    "url": "/Weather-app/static/css/main.8be78bbd.chunk.css"
+  },
+  {
+    "revision": "a30dd1d7435761ba6020",
+    "url": "/Weather-app/static/css/2.301cbc31.chunk.css"
+  },
+  {
+    "revision": "6827a32cebac00a483ec69afb9da27f0",
+    "url": "/Weather-app/index.html"
+  }
+];
